450282b65b27dc83be0bd644b38ee115850598b0
M1RK02 <picamirko02@gmail.com>
2021-03-15 09:29:49 +0100

    First GUI Version

--- Cisco.py
+++ Cisco.py
@@ -1,62 +1,64 @@
 #This program needs a text file named Cisco.txt with one meeting per line formatted like this "NAME CODE"
-import pyautogui, os, time
+import pyautogui, os, time, PySimpleGUI as sg
 
-def printSubjects(): #Print the list of possible meetings and return the max possible index
+def checkFile(): #Check if the file exist
+    try:
+        f = open('Cisco.txt')
+        return True
+    except IOError:
+        return False
+
+def listMeetings(): #Add a button for every meeting in the file
     file = open('Cisco.txt')
+    i=-1
     for i, line in enumerate(file):
-        subject = line.split()
-        print(f'{i+1}) {subject[0]}')
+        meeting = line.split()
+        buttons.append([sg.Button(meeting[0], key=i)])
     return i
 
 def getCode(): #Return the code of the selected meetings
     file = open('Cisco.txt')
     for i, line in enumerate(file):
         code = line.split()
-        if i+1 == choice:
+        if i == choice:
             return code[1]
 
-def checkInput(user_input): #Check if the input is valid
-    if not isNum(user_input): return False
-    user_input = int(user_input)
-    if not bounds(user_input): return False
-    return True
-
-def isNum(user_input): #Check if the input is a number
-    if not user_input.isnumeric():
-        print('This is not a valid number')
-        return False
-    else: return True
-
-def bounds(user_input): #Check if the input is in bounds
-    if user_input > max or user_input < 1:
-        print('This number is out of bounds')
-        return False
-    else: return True
+def enterMeeting():
+    pyautogui.hotkey('win', 'd') #Go to desktop
+    os.system(r'start "" "C:\Program Files (x86)\Webex\Webex\Applications\ptoneclk.exe"') #Open Cisco Webex
+    time.sleep(wait)
+    screen = pyautogui.size() #Get screen size
+    pyautogui.leftClick(screen[0]-250, 250) #Click code box
+    pyautogui.hotkey('ctrl', 'a')
+    pyautogui.typewrite(getCode()) #Write down the code
+    pyautogui.press('enter', presses=2, interval=wait) #Enter the meeting
 
 wait = 10
 
-max = printSubjects() + 1 #Print the list of possible meetings and save the max possible choice
+buttons = []
+
+if not checkFile():
+    layout = [[sg.Text('The file "Cisco.txt" does not exist')]]
+else:
+    i = listMeetings()
+    if i == -1:
+        layout = [[sg.Text('There are no meetings in the "Cisco.txt" file')]]
+    else:
+        layout = [ *buttons ] 
+
+
+window = sg.Window('CiscoAutoMeetings', layout, finalize=True, element_justification='Center')
+
+window.set_min_size((300,0))
+    
 
 while True:
-    choice = input('Select a meeting: ')
-    if not checkInput(choice): #Check if the input is valid
-        print('Please try again')
-        continue
-    choice = int(choice)
-    break
-
-pyautogui.hotkey('win', 'd') #Go to desktop
-
-os.system(r'start "" "C:\Program Files (x86)\Webex\Webex\Applications\ptoneclk.exe"') #Open Cisco Webex
-
-time.sleep(wait)
-
-screen = pyautogui.size() #Get screen size
-
-pyautogui.leftClick(screen[0]-250, 250) #Click code box
-
-pyautogui.hotkey('ctrl', 'a')
-
-pyautogui.typewrite(getCode()) #Write down the code
-
-pyautogui.press('enter', presses=2, interval=wait) #Enter the class
+    event, values = window.Read()
+    if event is None:
+        break
+    else:
+        choice = event
+        enterMeeting()
+        break
+    
+window.Close()
